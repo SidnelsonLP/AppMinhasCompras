ca201a8f5c648b345d0cc52f9181debc479e6355
Sidnelson Porto <sidnelsonlp@gmail.com>
2025-03-16 22:26:46 -0300

    adicionando, busca e soma ao app

--- AppMinhasCompras/Views/ListaProduto.xaml.cs
+++ AppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,24 @@
+using AppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace AppMinhasCompras.Views;
+
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+       List<Produto> tmp = await App.Db.GetAll();
+		tmp.ForEach(i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -16,5 +29,25 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private async void txt_serch_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+		lista.Clear();
+        List<Produto> tmp = await App.Db.Search(q);
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+		string msg = $"O total é {soma:C}";
+		DisplayAlert("Total de produtos", msg, "Ok");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
